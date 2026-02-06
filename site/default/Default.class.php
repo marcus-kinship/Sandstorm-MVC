fc8e2d556300c48cc740c848da69bf18657fb519
marcus_kinship <mackan.mac3@gmail.com>
2026-02-07 00:28:44 +0100

    Controller class
    Controller class

--- site/default/Default.class.php
+++ site/default/Default.class.php
@@ -1,16 +1,25 @@
 <?php
 
+/**
+ * DefaultController
+ * 
+ * Handles requests to the main/default pages of the application.
+ * Extends IController to inherit base controller functionality.
+ */
 class DefaultController extends IController
 {
 
     /**
-     * Start page
+     * Start page (Homepage)
+     * 
+     * This action is mapped to the root URL ("/") and renders
+     * the main starting view of the application.
      * 
      * @router / -> index
      */
     function index()
     {
-        // Load view
+        // Load the start view
         $this->load("default/start.php");
     }
 }
